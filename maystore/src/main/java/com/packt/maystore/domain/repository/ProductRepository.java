a3419e12c1d083f70ac579b85df357cfa31b5ba4
May Pyae Sone Nyein Win <nyeinwin04@gmail.com>
2025-06-02 18:45:23 +0630

    change package structure

--- maystore/src/main/java/com/packt/maystore/domain/repository/ProductRepository.java
+++
@@ -1,5 +0,0 @@
-package com.packt.maystore.domain.repository;
-
-public interface ProductRepository {
-
-}
--- maystore/src/main/java/com/pkg/maystore/MaystoreApplication.java
+++ maystore/src/main/java/com/pkg/maystore/MaystoreApplication.java
@@ -1,4 +1,4 @@
-package com.packt.maystore;
+package com.pkg.maystore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
